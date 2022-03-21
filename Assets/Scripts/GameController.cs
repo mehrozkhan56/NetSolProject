46425f8941aaa14316af6f42ef68da2a5296ebf3
mehrozkhan56 <102049331+mehrozkhan56@users.noreply.github.com>
2022-03-21 20:57:46 +0500

    Controller is a bit done.
    Controller is a bit done.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wrld;
+using Wrld.Space;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Transform camera = null;
+
+    void Start()
+    {
+        //Api.Instance.CameraApi.MoveTo(cameraLocation, distanceFromInterest: 400, headingDegrees: 0, tiltDegrees: 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeBox(LatLong latLong)
+    {
+        //var ray = Api.Instance.SpacesApi.LatLongToVerticallyDownRay(latLong);
+        //LatLongAltitude buildingIntersectionPoint;
+        //var didIntersectBuilding = Api.Instance.BuildingsApi.TryFindIntersectionWithBuilding(ray, out buildingIntersectionPoint);
+        //if (didIntersectBuilding)
+        //{
+        //    var boxAnchor = Instantiate(boxPrefab) as GameObject;
+        //    boxAnchor.GetComponent<GeographicTransform>().SetPosition(buildingIntersectionPoint.GetLatLong());
+
+        //    var box = boxAnchor.transform.GetChild(0);
+        //    box.localPosition = Vector3.up * (float)buildingIntersectionPoint.GetAltitude();
+        //    Destroy(boxAnchor, 2.0f);
+        //}
+    }
+}
---
+++ Assets/Scripts/HeliController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class HeliController : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    Vector2 movementValue = Vector2.zero;
+    public Transform heliObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        movementValue.x = Input.GetAxis("Horizontal");
+        movementValue.y = Input.GetAxis("Vertical");
+        MovementHandling();
+        TiltHandling();
+    }
+    
+    public float rotateSpeed = 3f;
+    float turn = 0.0f;
+    private void MovementHandling()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * movementSpeed * movementValue.y;
+    }
+
+    float currentZRotation = 0.0f;
+    public float rotationSpeed = 1000.0f;
+    public float minRotation = -30.0f;
+    public float maxRotation = 30.0f;
+    private void TiltHandling()
+    {
+        if (movementValue.x == 0)
+            currentZRotation = Mathf.MoveTowards(currentZRotation, 0, Time.deltaTime * rotationSpeed);
+        else
+            currentZRotation += movementValue.x * rotationSpeed * Time.deltaTime;
+        currentZRotation = Mathf.Clamp(currentZRotation, minRotation, maxRotation);
+        heliObject.localEulerAngles = new Vector3(0, 0, -currentZRotation);
+    }
+}
